cfe503f1b83a1abf0be5befbdf9aa2d518a3a34b
USER10 <USER10@DESKTOP-FRS69SE>
2020-01-14 15:21:00 +0200

    add db

---
+++ Cinema/DAL/Entities/Description.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUDCore
+{
+    public partial class Description
+    {
+        public int Id { get; set; }
+        public string Genre { get; set; }
+        public string ReleaseDate { get; set; }
+        public string Director { get; set; }
+        public string Cast { get; set; }
+    }
+}
