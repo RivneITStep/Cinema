c03e24321a1f0c15eb91cbb65e9663e6780ed63d
Максим Іскандіров <maksimiskandirov2.0@gmail.com>
2020-02-03 17:09:42 +0300

    style

--- CRUDCore/ClientApp/src/components/containers/DefaultLayout/NavMenu.js
+++ CRUDCore/ClientApp/src/components/containers/DefaultLayout/NavMenu.js
@@ -26,15 +26,65 @@ export class NavMenu extends React.Component {
     const { isAuthenticated, user } = this.props;
     return (
       <header>
+         <div id="base_menu">
+                    <div className="flip">
+                        <div className="flip-inner">
+                            <div className="flip-front">
+                                <Link className="ped" to="/"></Link>
+                            </div>
+                            <div className="flip-back">
+                                <Link className="ped" to="/"></Link>
+                            </div>
+                        </div>
+                    </div>
+                    {/* ////// */}
+                        <input type="checkbox" id="toggle" />
+                        <label className="label" htmlFor="toggle">Genres</label>
+
+                        <dialog >
+                            <li>аниме</li>
+                            <li>биографический</li>
+                            <li>боевик</li>
+                            <li>вестерн</li>
+                            <li>военный</li>
+                            <li>детектив</li>
+                            <li>детский</li>
+                            <li>документальный</li>
+                            <li>драма</li>
+                            <li>исторический</li>
+                            <li>кинокомикс</li>
+                            <li>комедия</li>
+                            <li>короткометражный</li>
+                            <li>криминал</li>
+                            <li>мелодрама</li>
+                            <li>мистика</li>
+                            <li>мультфильм</li>
+                            <li>научный</li>
+                            <li>приключения</li>
+                            <li>семейный</li>
+                            <li>триллер</li>
+                            <li>ужасы</li>
+                            <li>фантастика</li>
+                            <li>фэнтези</li>
+                            <label className="close" htmlFor="toggle">close overlay</label>
+                        </dialog>
+
+                    {/* /////// */}
+                    <div className="search-button">
+                        <div className="container">
+                            <span className="lupa">
+                                <i className="fa fa-search"></i>
+                            </span>
+                            <input type="search" id="search" placeholder="Поиск по сайту" />
+                        </div>
+                    </div>
+                </div>
         <div className="hamburger-menu">
           <input id="menu__toggle" type="checkbox" />
           <label className="menu__btn" htmlFor="menu__toggle">
             <span></span>
           </label>
           <ul className="menu__box">
-            <li className="nav-item">
-              <img src="../../../public/favico_1.ico" alt="asd" />
-            </li>
             <li className="nav-item">
               <Link className="menu__item" to="/home">CiNeMa</Link>
             </li>
