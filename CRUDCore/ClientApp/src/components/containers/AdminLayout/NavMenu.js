16b87407ab79d770aaa6329e5650d4d6d9373509
Максим Іскандіров <maksimiskandirov2.0@gmail.com>
2020-02-06 17:32:36 +0300

    fix visual bugs

--- CRUDCore/ClientApp/src/components/containers/AdminLayout/NavMenu.js
+++ CRUDCore/ClientApp/src/components/containers/AdminLayout/NavMenu.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Admin.css';
-import $ from "jquery";
+// import $ from "jquery";
 
 export class NavMenu extends React.Component {
   constructor(props) {
@@ -26,119 +26,118 @@ export class NavMenu extends React.Component {
         </div>
         <div className="Main">
 
-          <div class="pie-container">
-            <div id="safari" class="pie"></div>
-            <div id="ie" class="pie"></div>
-            <div id="firefox" class="pie"></div>
-            <div id="chrome" class="pie"></div>
+          <div className="pie-container">
+            <div id="notregist" className="pie"></div>
+            <div id="ie" className="pie"></div>
+            <div id="something" className="pie"></div>
+            <div id="firefox" className="pie"></div>
+            <div id="regist" className="pie"></div>
           </div>
-
-          <div class="stats">
+          <h2>Статистика користувачів сайту</h2>
+          <div className="stats">
             <ul>
-              <li data-name="Chrome">Chrome</li>
+              <li data-name="Chrome">Зареєстрованих</li>
               <li data-name="Firefox">Firefox</li>
               <li data-name="Internet Explorer">Internet Explorer</li>
-              <li data-name="Safari">Safari</li>
+              <li data-name="Safari">Не зареєстрованих</li>
             </ul>
           </div>
-          <div class="wrap">
-            <div class="barra barra1">
+          <div className="wrap">
+            <div className="barra barra1">
             </div>
-            <div class="barra barra2">
+            <div className="barra barra2">
             </div>
-            <div class="barra barra3">
+            <div className="barra barra3">
             </div>
-            <div class="barra barra4">
+            <div className="barra barra4">
             </div>
-            <div class="barra barra5">
-            </div>
-            <div class="pointer">Height:
-            <span class="data-height"></span>
+            <div className="pointer">Height:
+            <span className="data-height"></span>
             </div>
           </div>
-          <div class="bar-chart">
-	<div class="y-axis">
-		<div class="label">70.00%</div>
-		<div class="label">60.00%</div>
-		<div class="label">50.00%</div>
-		<div class="label">40.00%</div>
-		<div class="label">30.00%</div>
-		<div class="label">20.00%</div>
-		<div class="label">10.00%</div>
-		<div class="label">0.00%</div>
+          <div className="bar-chart">
+	<div className="y-axis">
+		<div className="label">70.00%</div>
+		<div className="label">60.00%</div>
+		<div className="label">50.00%</div>
+		<div className="label">40.00%</div>
+		<div className="label">30.00%</div>
+		<div className="label">20.00%</div>
+		<div className="label">10.00%</div>
+		<div className="label">0.00%</div>
 	</div>
-	<div class="overflow-wrap">
-		<div class="indicator"></div>
-		<div class="sub-grid">
-			<div class="x-axis">
-				<div class="label">School #1</div>
-				<div class="label">School #2</div>
-				<div class="label">School #3</div>
-				<div class="label">School #4</div>
-				<div class="label">School #5</div>
-				<div class="label">School #6</div>
-				<div class="label">School #7</div>
-				<div class="label">School #8</div>
-				<div class="label">School #9</div>
-				<div class="label">School #10</div>
-				<div class="label">School #11</div>
-				<div class="label">School #12</div>
+	<div className="overflow-wrap">
+		<div className="indicator"></div>
+		<div className="sub-grid">
+			<div className="x-axis">
+				<div className="label">School #1</div>
+				<div className="label">School #2</div>
+				<div className="label">School #3</div>
+				<div className="label">School #4</div>
+				<div className="label">School #5</div>
+				<div className="label">School #6</div>
+				<div className="label">School #7</div>
+				<div className="label">School #8</div>
+				<div className="label">School #9</div>
+				<div className="label">School #10</div>
+				<div className="label">School #11</div>
+				<div className="label">School #12</div>
 			</div>
-			<div class="chart">
-				<div class="gridRow gridRow-1"></div>
-				<div class="gridRow gridRow-2"></div>
-				<div class="gridRow gridRow-3"></div>
-				<div class="gridRow gridRow-4"></div>
-				<div class="gridRow gridRow-5"></div>
-				<div class="gridRow gridRow-6"></div>
-				<div class="gridRow gridRow-7"></div>
-				<div class="barGroup barGroup-1">
-					<div class="bar bar-1"></div>
+			<div className="chart">
+				<div className="gridRow gridRow-1"></div>
+				<div className="gridRow gridRow-2"></div>
+				<div className="gridRow gridRow-3"></div>
+				<div className="gridRow gridRow-4"></div>
+				<div className="gridRow gridRow-5"></div>
+				<div className="gridRow gridRow-6"></div>
+				<div className="gridRow gridRow-7"></div>
+				<div className="barGroup barGroup-1">
+					<div className="bar bar-1"></div>
 				</div>
-				<div class="barGroup barGroup-2">
-					<div class="bar bar-1"></div>
+				<div className="barGroup barGroup-2">
+					<div className="bar bar-1"></div>
 				</div>
-				<div class="barGroup barGroup-3">
-					<div class="bar bar-1"></div>
+				<div className="barGroup barGroup-3">
+					<div className="bar bar-1"></div>
 				</div>
-				<div class="barGroup barGroup-4">
-					<div class="bar bar-1"></div>
+				<div className="barGroup barGroup-4">
+					<div className="bar bar-1"></div>
 				</div>
-				<div class="barGroup barGroup-5">
-					<div class="bar bar-1"></div>
+				<div className="barGroup barGroup-5">
+					<div className="bar bar-1"></div>
 				</div>
-				<div class="barGroup barGroup-6">
-					<div class="bar bar-1"></div>
+				<div className="barGroup barGroup-6">
+					<div className="bar bar-1"></div>
 				</div>
-				<div class="barGroup barGroup-7">
-					<div class="bar bar-1"></div>
-					<div class="bar bar-2"></div>
-					<div class="bar bar-3"></div>
+				<div className="barGroup barGroup-7">
+					<div className="bar bar-1"></div>
+					<div className="bar bar-2"></div>
+					<div className="bar bar-3"></div>
 				</div>
-				<div class="barGroup barGroup-8">
-					<div class="bar bar-1"></div>
-					<div class="bar bar-2"></div>
-					<div class="bar bar-3"></div>
+				<div className="barGroup barGroup-8">
+					<div className="bar bar-1"></div>
+					<div className="bar bar-2"></div>
+					<div className="bar bar-3"></div>
 				</div>
-				<div class="barGroup barGroup-9">
-					<div class="bar bar-1"></div>
-					<div class="bar bar-2"></div>
-					<div class="bar bar-3"></div>
+				<div className="barGroup barGroup-9">
+					<div className="bar bar-1"></div>
+					<div className="bar bar-2"></div>
+					<div className="bar bar-3"></div>
 				</div>
-				<div class="barGroup barGroup-10">
-					<div class="bar bar-1"></div>
-					<div class="bar bar-2"></div>
-					<div class="bar bar-3"></div>
+				<div className="barGroup barGroup-10">
+					<div className="bar bar-1"></div>
+					<div className="bar bar-2"></div>
+					<div className="bar bar-3"></div>
 				</div>
-				<div class="barGroup barGroup-11">
-					<div class="bar bar-1"></div>
-					<div class="bar bar-2"></div>
-					<div class="bar bar-3"></div>
+				<div className="barGroup barGroup-11">
+					<div className="bar bar-1"></div>
+					<div className="bar bar-2"></div>
+					<div className="bar bar-3"></div>
 				</div>
-				<div class="barGroup barGroup-12">
-					<div class="bar bar-1"></div>
-					<div class="bar bar-2"></div>
-					<div class="bar bar-3"></div>
+				<div className="barGroup barGroup-12">
+					<div className="bar bar-1"></div>
+					<div className="bar bar-2"></div>
+					<div className="bar bar-3"></div>
 				</div>
 			</div>
 		</div>
