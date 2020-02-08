f12ff9471c11d0b9ecaff5948bf57b396e5d5220
USER10 <USER10@DESKTOP-FRS69SE>
2020-02-08 13:17:22 +0200

    fix bag

--- CRUDCore/ClientApp/src/components/Home/Home.js
+++ CRUDCore/ClientApp/src/components/Home/Home.js
@@ -18,58 +18,58 @@ class Home extends Component {
         var Film_names = ['Телохранитель', 'Макс Стіл', 'Исходний код'];
         return (
             <div className="main">
-                <div id="container">
-                    <div id="container2">
-                        <div className="box one">
-                            <div>
-                                <div className="small" id="web">
-                                    <div>
-                                        <p align="center">{Film_names[0]}</p>
-                                    </div>
-                                    <Link className="foto" to="/Application"></Link>
-                                    <div>
-                                        <p align="center">Год:&nbsp;2010</p>
-                                    </div>
+            <div id="container">
+                <div id="container2">
+                    <div className="box one">
+                        <div>
+                            <div className="small" id="web">
+                                <div>
+                                    <p align="center">{Film_names[0]}</p>
+                                </div>
+                                <Link className="foto" to="/Application"></Link>
+                                <div>
+                                    <p align="center">Год:&nbsp;2010</p>
                                 </div>
                             </div>
                         </div>
-                        <div className="box two">
-                            <div>
-                                <div className="small" id="web">
-                                    <div>
-                                    <p align="center">{Film_names[1]}</p>
-                                    </div>
-                                    <Link className="foto1" to="/film" ></Link>
-                                    <div>
-                                        <p align="center"> Год:&nbsp;2016</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="box three">
-                            <div>
-                            <div>
-                                <div className="small" id="web">
-                                    <div>
-                                        <p align="center"> {Film_names[2]}</p>
-                                    </div>
-                                    <Link className="foto2" to="/film" ></Link>
-                                    <div>
-                                    </div>
-                                </div>
-                            </div>
-                            </div>
-                        </div>
-                        <div className="box four"><div>тест</div></div>
-                        <div className="box two"><div>1</div></div>
-                        <div className="box three"><div>2</div></div>
-                        <div className="box four"><div>3</div></div>
-                        <div className="box two"><div>4</div></div>
-                        <div className="box three"><div>5</div></div>
-                        <div className="box four"><div>6</div></div>
                     </div>
+                    <div className="box two">
+                        <div>
+                            <div className="small" id="web">
+                                <div>
+                                <p align="center">{Film_names[1]}</p>
+                                </div>
+                                <Link className="foto1" to="/film" ></Link>
+                                <div>
+                                    <p align="center"> Год:&nbsp;2016</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="box three">
+                        <div>
+                        <div>
+                            <div className="small" id="web">
+                                <div>
+                                    <p align="center"> {Film_names[2]}</p>
+                                </div>
+                                <Link className="foto2" to="/film2" ></Link>
+                                <div>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="box four"><div>тест</div></div>
+                    <div className="box two"><div>1</div></div>
+                    <div className="box three"><div>2</div></div>
+                    <div className="box four"><div>3</div></div>
+                    <div className="box two"><div>4</div></div>
+                    <div className="box three"><div>5</div></div>
+                    <div className="box four"><div>6</div></div>
                 </div>
             </div>
+        </div>
         );
     }
 }
--- CRUDCore/ClientApp/src/routes/defaultRoutes.js
+++ CRUDCore/ClientApp/src/routes/defaultRoutes.js
@@ -4,11 +4,18 @@ const Home = React.lazy(() => import("../components/Home/Home"));
 const Login = React.lazy(() => import("../components/Login/LoginPage"));
 const Registration = React.lazy(() => import("../components/Registration/RegistrationPage"));
 const Profile = React.lazy(() => import("../components/UserProfile/userprofile"));
+const Application = React.lazy(() => import("../components/demo/Application"));
+const film = React.lazy(() => import("../components/demo/film"));
+const film2 = React.lazy(() => import("../components/demo/film2"));
+
 
 const defaultRoutes = [
     { path: '/home', exact: true, name: 'Головна', component: Home  },
     { path: '/login', exact: true, name: 'Вхід', component: Login  },
     { path: '/registration', exact: true, name: 'Вхід', component: Registration  },
     { path: '/profile', exact: true, name: 'Профіль', component: Profile  },
+    { path: '/Application', exact: true, name: 'фільм', component: Application  },
+    { path: '/film', exact: true, name: 'фільм', component: film  },
+    { path: '/film2', exact: true, name: 'фільм', component: film2  },
 ];
 export default defaultRoutes;
