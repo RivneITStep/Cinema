e30e75ac6a0336e4956d6ac733398831c68662c5
USER10 <USER10@DESKTOP-FRS69SE>
2020-02-29 15:26:29 +0200

    add film

--- CRUDCore/ClientApp/src/components/Add_Films/Add_film.js
+++ CRUDCore/ClientApp/src/components/Add_Films/Add_film.js
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React from 'react';
 import './Admin.css';
 class Add_film extends React.Component {
 
@@ -12,45 +12,71 @@ class Add_film extends React.Component {
 
   render() {
     return (
-      <div className="centeradm">
-        <div>
-          <input type="text"
-            className="admintext"
-            id="d1"
-            name="Name"
-            placeholder="Name"
-          />
+      <div className="cent">
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1">Назва фільма</span>
+          </div>
+          <input type="text" class="form-control" placeholder="Назва фільма" aria-label="Username" aria-describedby="basic-addon1" />
         </div>
-        <div>
-          <input type="number"
-            className="admintext"
-            id="d2"
-            name="Year"
-            placeholder="Year"
-          />
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text">Вибрати фото</span>
+          </div>
+          <div class="custom-file">
+            <input type="file" class="custom-file-input" id="inputGroupFile01" />
+            <label class="custom-file-label" for="inputGroupFile01">Вибрати фото</label>
+          </div>
         </div>
-        <div>
-          <input type="text"
-            className="admintext"
-            id="d3"
-            name="country"
-            placeholder="Country"
-          />
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1">Режисер</span>
+          </div>
+          <input type="text" class="form-control" placeholder="Режисер" aria-label="Режисер" aria-describedby="basic-addon2" />
         </div>
-        <div>
-          <input type="text"
-            className="admintext"
-            id="d4"
-            name="director"
-            placeholder="Director"
-          />
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1">Актори </span>
+          </div>
+          <input type="text" class="form-control" placeholder="Актори" aria-label="Режисер" aria-describedby="basic-addon2" />
         </div>
-        {/* id="d2" id="d3" d="d4" */}
-        <button id="d1"  onClick={this.logconsole} type="submit">add film</button>
-        
-        <div className="panel" >
-         <label className="lb"id="d1"></label>
+        <label for="basic-url"></label>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon3">Силка на фільм</span>
+          </div>
+          <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3" />
         </div>
+
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text">Тривалість</span>
+          </div>
+          <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)" />
+          <div class="input-group-append">
+            <span class="input-group-text"></span>
+          </div>
+        </div>
+
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <span class="input-group-text">Опис</span>
+          </div>
+          <textarea class="form-control" aria-label="With textarea"></textarea>
+        </div>
+        <br></br>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <label class="input-group-text" for="inputGroupSelect01">розташування</label>
+          </div>
+          <select class="custom-select" id="inputGroupSelect01">
+            <option selected>В якій кардці буде знаходитися</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+        </div>
+        <button type="button" class="btn btn-primary btn-lg">Додати</button>
       </div>
     )
   }
